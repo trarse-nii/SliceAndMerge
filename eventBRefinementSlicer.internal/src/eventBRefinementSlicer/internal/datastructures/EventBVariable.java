26caba31ab378fc0a78e3ea17262a2e3885ec549
Aivar Kripsaar <aivar.kripsaar@rwth-aachen.de>
2015-11-05 17:59:53 +0900

    Changes the way a variables name is extracted

--- eventBRefinementSlicer.internal/src/eventBRefinementSlicer/internal/datastructures/EventBVariable.java
+++ eventBRefinementSlicer.internal/src/eventBRefinementSlicer/internal/datastructures/EventBVariable.java
@@ -1,33 +1,32 @@
 package eventBRefinementSlicer.internal.datastructures;
 
-import org.eventb.core.EventBAttributes;
 import org.eventb.core.IVariable;
 import org.rodinp.core.RodinDBException;
 
 public class EventBVariable extends EventBAttribute {
-	
+
 	private static final String TYPE = "VARIABLE";
-	
-	public EventBVariable(String label, String comment, EventBUnit parent){
+
+	public EventBVariable(String label, String comment, EventBUnit parent) {
 		super(label, comment, parent);
 	}
-	
-	public EventBVariable(IVariable variable, EventBUnit parent) throws RodinDBException{
+
+	public EventBVariable(IVariable variable, EventBUnit parent) throws RodinDBException {
 		super(parent);
 		String label = "";
 		String comment = "";
-		if (variable.hasAttribute(EventBAttributes.IDENTIFIER_ATTRIBUTE)){
-			label = variable.getAttributeValue(EventBAttributes.IDENTIFIER_ATTRIBUTE);
+		if (variable.hasIdentifierString()) {
+			label = variable.getIdentifierString();
 		}
-		if (variable.hasComment()){
+		if (variable.hasComment()) {
 			comment = variable.getComment();
 		}
 		this.label = label;
 		this.comment = comment;
 	}
-	
+
 	@Override
-	public String toString(){
-		return TYPE + ": [" + (selected ? "x" : " ") + "] " + label + " (" + comment + ")"; 
+	public String toString() {
+		return TYPE + ": [" + (selected ? "x" : " ") + "] " + label + " (" + comment + ")";
 	}
 }
