365c1008adda65ea4e5798409262c15c473076cb
Aivar Kripsaar <aivar.kripsaar@rwth-aachen.de>
2016-01-07 14:14:19 +0900

    Adds parameters to guard dependency analysis

--- eventBRefinementSlicer.internal/src/eventBRefinementSlicer/internal/datastructures/EventBGuard.java
+++ eventBRefinementSlicer.internal/src/eventBRefinementSlicer/internal/datastructures/EventBGuard.java
@@ -1,7 +1,13 @@
 package eventBRefinementSlicer.internal.datastructures;
 
+import java.util.Set;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eventb.core.IGuard;
 import org.eventb.core.ISCGuard;
+import org.eventb.core.ast.FreeIdentifier;
+import org.eventb.core.ast.ITypeEnvironment;
+import org.eventb.core.ast.Predicate;
 import org.rodinp.core.RodinDBException;
 
 /**
@@ -27,6 +33,28 @@ public class EventBGuard extends EventBCondition {
 	public EventBGuard(IGuard guard, ISCGuard scGuard, EventBEvent parentEvent, EventBUnit parentUnit) throws RodinDBException {
 		super(guard, scGuard, parentUnit);
 		this.parentEvent = parentEvent;
+	}
+
+	@Override
+	public Set<EventBAttribute> calculateDependees() {
+		Set<EventBAttribute> occurredAttributes = super.calculateDependees();
+		// Also check parameters
+		Predicate pred;
+		try {
+			ITypeEnvironment typeEnvironment = parent.getTypeEnvironment();
+			pred = getScElement().getPredicate(typeEnvironment);
+			for (FreeIdentifier freeIdentifier : pred.getFreeIdentifiers()) {
+				for (EventBParameter parameter : parentEvent.getParameters()) {
+					if (parameter.getLabel().equals(freeIdentifier.getName())) {
+						occurredAttributes.add(parameter);
+						break;
+					}
+				}
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return occurredAttributes;
 	}
 
 	@Override
